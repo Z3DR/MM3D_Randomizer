7ed808661f097ce952a773ba77d93df871b7e60c
pizza1398 <259104093+pizza1398@users.noreply.github.com>
2026-03-02 19:16:52 +0100

    Directory migration + info screen (from OOT3DR) (#152)
    - Add directory creation info screen
    - Transfer and save presets to /MM3DR/Presets/
    - Save logs in /MM3DR/Spoiler_Logs/

--- source/include/preset.hpp
+++ source/include/preset.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
+#include <3ds.h>
+
 #include <string>
 #include <vector>
 
 enum class OptionCategory;
 
-bool CreatePresetDirectories();
+void CreatePresetDirectories(FS_Archive sdmcArchive);
 std::vector<std::string> GetSettingsPresets();
 bool SavePreset(std::string_view presetName, OptionCategory category);
 bool LoadPreset(std::string_view presetName, OptionCategory category);
