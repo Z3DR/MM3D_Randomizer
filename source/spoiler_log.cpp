b4e61b0a4e226e34b95a24d82c8cbac067e085e1
pizza1398 <259104093+pizza1398@users.noreply.github.com>
2026-03-02 16:53:08 +0100

    Update utils to match OOT3DR
    - Convert RemoveLineBreaks to SanitizedString
    - Move CopyFile to utils

--- source/spoiler_log.cpp
+++ source/spoiler_log.cpp
@@ -455,7 +455,7 @@ static void WriteSettings(tinyxml2::XMLDocument& spoilerLog, const bool printAll
       for (const Option* setting : *menu->settingsList) {
         if (printAll || (!setting->IsHidden() && setting->IsCategory(OptionCategory::Setting))) {
           auto node = parentNode->InsertNewChildElement("setting");
-          node->SetAttribute("name", RemoveLineBreaks(setting->GetName()).c_str());
+          node->SetAttribute("name", SanitizedString(setting->GetName()).c_str());
           node->SetText(setting->GetSelectedOptionText().c_str());
         }
       }
@@ -474,7 +474,7 @@ static void WriteExcludedLocations(tinyxml2::XMLDocument& spoilerLog) {
     }
 
     tinyxml2::XMLElement* node = spoilerLog.NewElement("location");
-    node->SetAttribute("name", RemoveLineBreaks(location->GetName()).c_str());
+    node->SetAttribute("name", SanitizedString(location->GetName()).c_str());
     parentNode->InsertEndChild(node);
   }
 
@@ -564,7 +564,7 @@ static void WriteEnabledTricks(tinyxml2::XMLDocument& spoilerLog) {
     }
 
     auto node = parentNode->InsertNewChildElement("trick");
-    node->SetAttribute("name", RemoveLineBreaks(setting->GetName()).c_str());
+    node->SetAttribute("name", SanitizedString(setting->GetName()).c_str());
   }
 
   if (!parentNode->NoChildren()) {
--- source/utils.cpp
+++ source/utils.cpp
@@ -1,7 +1,57 @@
+#include <algorithm>
+#include <memory>
+#include <vector>
+
 #include "utils.hpp"
 
-// Removes any line breaks from s.
-std::string RemoveLineBreaks(std::string s) {
-  s.erase(std::remove(s.begin(), s.end(), '\n'), s.end());
+std::string SanitizedString(std::string s) {
+  // Remove leading spaces
+  while (s.size() > 0 && s[0] == ' ') {
+    s.erase(0, 1);
+  }
+
+  // Replace line breaks with spaces
+  std::replace(s.begin(), s.end(), '\n', ' ');
+
+  // Remove consecutive spaces
+  while (s.find("  ") != std::string::npos) {
+    s.erase(s.find("  "), 1);
+  }
+
   return s;
+}
+
+using FILEPtr = std::unique_ptr<FILE, decltype(&std::fclose)>;
+
+bool CopyFile(FS_Archive sdmcArchive, const char* dst, const char* src) {
+  Result res = 0;
+  Handle outFile;
+  u32 bytesWritten = 0;
+  // Delete dst if it exists
+  FSUSER_DeleteFile(sdmcArchive, fsMakePath(PATH_ASCII, dst));
+
+  // Open dst destination
+  if (!R_SUCCEEDED(res = FSUSER_OpenFile(&outFile, sdmcArchive, fsMakePath(PATH_ASCII, dst),
+                                          FS_OPEN_WRITE | FS_OPEN_CREATE, 0))) {
+    return false;
+  }
+
+  if (auto file = FILEPtr{ std::fopen(src, "r"), std::fclose }) {
+    // obtain file size
+    fseek(file.get(), 0, SEEK_END);
+    const auto lSize = static_cast<size_t>(ftell(file.get()));
+    rewind(file.get());
+
+    // copy file into the buffer
+    std::vector<char> buffer(lSize);
+    fread(buffer.data(), 1, buffer.size(), file.get());
+
+    // Write the buffer to final destination
+    if (!R_SUCCEEDED(res = FSFILE_Write(outFile, &bytesWritten, 0, buffer.data(), buffer.size(), FS_WRITE_FLUSH))) {
+      return false;
+    }
+  }
+
+  FSFILE_Close(outFile);
+  return true;
 }
